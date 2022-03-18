b05225cc951a88e361bb8310898f0df3c79cb9b0
Axel DENIS <axel.denis@epitech.eu>
2022-03-18 22:21:02 +0100

    [EDIT] Splitted loose_menu function

--- src/lose_menu.c
+++ src/lose_menu.c
@@ -13,18 +13,15 @@
 #include "button.h"
 #include "events.h"
 
-button *create_buttons_lose(env_t *env)
+
+void setup_buttons(env_t *env, button *buttons)
 {
-    button *buttons = malloc(sizeof(button) * 2);
     sfIntRect square = create_rect(0, 0, 6065 / 3, 833);
 
-    //buttons[0] = create_button(VC{0.2, 0.2}, VC{258, WINDOW_HEIGHT / 2 - 100}, sfTrue);
-    //buttons[1] = create_button(VC{0.2, 0.2}, VC{258, WINDOW_HEIGHT / 2 + 100}, sfTrue);
-    buttons[0] = create_button(VC{0.2, 0.2}, VC{WINDOW_WIDTH / 2 - ((6065/3)*0.2)/2, WINDOW_HEIGHT / 2 - 100}, sfTrue);
-    buttons[1] = create_button(VC{0.2, 0.2}, VC{WINDOW_WIDTH / 2 - ((6065/3)*0.2)/2, WINDOW_HEIGHT / 2 + 100}, sfTrue);
     for (int i = 0; i < 2; i++) {
         setup_button_texture(&(buttons[i]), &square, "img/Blue_button.png");
-        setup_button_sounds(&(buttons[i]), "sounds/click.ogg", "sounds/hover.ogg", env);
+        setup_button_sounds(&(buttons[i]),
+            "sounds/click.ogg", "sounds/hover.ogg", env);
     }
     if (env->langue[0] == 'E') {
         setup_button_text(&(buttons[0]), "Main menu", "font/Xero.ttf", 40);
@@ -35,7 +32,38 @@ button *create_buttons_lose(env_t *env)
     }
     for (int i = 0; i < 2; i++)
         center_button_text(&(buttons[i]));
+}
+
+button *create_buttons_lose(env_t *env)
+{
+    button *buttons = malloc(sizeof(button) * 2);
+
+    buttons[0] = create_button(VC{0.2, 0.2}, VC{WINDOW_WIDTH / 2
+        - ((6065/3)*0.2)/2, WINDOW_HEIGHT / 2 - 100}, sfTrue);
+    buttons[1] = create_button(VC{0.2, 0.2}, VC{WINDOW_WIDTH / 2
+        - ((6065/3)*0.2)/2, WINDOW_HEIGHT / 2 + 100}, sfTrue);
+    setup_buttons(env, buttons);
     return (buttons);
+}
+
+int render_loose_menu(SFWIN window, int *keys, button *buttons, env_t *env)
+{
+    int open = 1;
+    object backfr = create_object("img/lose.jpg", VC{0, 0}, VC{1, 1});
+    object backen = create_object("img/lose.jpg", VC{0, 0}, VC{1, 1});
+
+    sfRenderWindow_clear(window, sfBlack);
+    get_events(window, keys);
+    if (keys[sfKeyEscape] == 3)
+        open = 0;
+    if (is_pressed(buttons[0], window, keys))
+        open = 0;
+    if (is_pressed(buttons[1], window, keys))
+        sfRenderWindow_close(window);
+    display_background(window, backfr, backen, env);
+    for (int i = 0; i < 2; i++)
+        display_button(window, &(buttons[i]), keys);
+    return open;
 }
 
 void lose_menu(sfRenderWindow *window, object mouse, int *keys, env_t *env)
@@ -43,8 +71,6 @@ void lose_menu(sfRenderWindow *window, object mouse, int *keys, env_t *env)
     int open = 1;
     button *buttons = create_buttons_lose(env);
     text texte;
-    object backfr = create_object("img/lose.jpg", VC{0, 0}, VC{1, 1});
-    object backen = create_object("img/lose.jpg", VC{0, 0}, VC{1, 1});
 
     if (env->langue[0] == 'E') {
         texte = setup_text("   Lose", "font/o_driftbold.ttf", 150);
@@ -52,17 +78,7 @@ void lose_menu(sfRenderWindow *window, object mouse, int *keys, env_t *env)
         texte = setup_text(" Defaite", "font/o_driftbold.ttf", 150);
     sfText_setPosition(texte.text, VC{670, 200});
     while (sfRenderWindow_isOpen(window) && open) {
-        sfRenderWindow_clear(window, sfBlack);
-        get_events(window, keys);
-        if (keys[sfKeyEscape] == 3)
-            open = 0;
-        if (is_pressed(buttons[0], window, keys))
-            open = 0;
-        if (is_pressed(buttons[1], window, keys))
-            sfRenderWindow_close(window);
-        display_background(window, backfr, backen, env);
-        for (int i = 0; i < 2; i++)
-            display_button(window, &(buttons[i]), keys);
+        open = render_loose_menu(window, keys, buttons, env);
         sfRenderWindow_drawText(window, texte.text, NULL);
         update_mouse_cursor(window, mouse, env->tempo);
         sfRenderWindow_display(window);
