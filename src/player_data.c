17f434761489f0aeefd4141f0b3253fe23ebb671
Aillet <arthur.aillet@epitech.eu>
2022-03-20 23:23:30 +0100

    [EDIT] tweak values of ennemies

--- src/player_data.c
+++ src/player_data.c
@@ -18,8 +18,8 @@ int create_game(env_t *env)
     env->c_game.player_stats.health = 100;
     env->c_game.player_stats.energy = 100;
     env->c_game.player_stats.steel = 100;
-    env->c_game.player_stats.energy_income = 1;
-    env->c_game.player_stats.steel_income = 1;
+    env->c_game.player_stats.energy_income = 3;
+    env->c_game.player_stats.steel_income = 3;
     env->c_game.player_stats.wave = 1;
     env->c_game.enemies = NULL;
     env->c_game.turrets = NULL;
